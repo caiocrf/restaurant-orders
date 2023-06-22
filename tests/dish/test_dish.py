63f1b89c0199933369e9b45eb85535c4089177d7
caio pinho <icandoit2026@gmail.com>
2023-06-22 15:59:40 -0300

    req 1 and 2

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,35 @@
+import pytest
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient
 
 
-# Req 2
 def test_dish():
-    pass
+    ovo = Ingredient("ovo")
+    quantidade_ovo = 10
+    pizza = "Pizza"
+    pizza_price = 39.20
+    hamburger = "Hamburger"
+    hamburger_price = 20.20
+
+    pizzaa_dish = Dish(pizza, pizza_price)
+    assert pizzaa_dish.name == pizza
+    assert repr(pizzaa_dish) == f"Dish('{pizza}', R${pizza_price:.2f})"
+
+    pizzaa_dish.add_ingredient_dependency(ovo, quantidade_ovo)
+    assert pizzaa_dish.recipe.get(ovo) == quantidade_ovo
+    assert pizzaa_dish.get_ingredients() == {ovo}
+
+    assert pizzaa_dish.get_restrictions() == ovo.restrictions
+
+    pizzaa_second_dish = Dish(pizza, pizza_price)
+    assert pizzaa_dish.__hash__() == pizzaa_second_dish.__hash__()
+    assert pizzaa_dish == pizzaa_second_dish
+
+    hamburger_dishh = Dish(hamburger, hamburger_price)
+    assert pizzaa_dish.__hash__() != hamburger_dishh.__hash__()
+    assert pizzaa_dish != hamburger_dishh
+
+    with pytest.raises(TypeError):
+        assert Dish(pizza, "")
+    with pytest.raises(ValueError):
+        assert Dish(pizza, 0)
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,16 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
-
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 def test_ingredient():
-    pass
+    ovo = Ingredient("ovo")
+    assert ovo.name == "ovo"
+    assert ovo.restrictions == {Restriction.ANIMAL_DERIVED}
+    assert repr(ovo) == "Ingredient('ovo')"
+
+    segundo_ovo = Ingredient("ovo")
+    assert ovo.__hash__() == segundo_ovo.__hash__()
+    assert ovo == segundo_ovo
+
+    frango = Ingredient("frango")
+    assert ovo.__hash__() != frango.__hash__()
+    assert ovo != frango
